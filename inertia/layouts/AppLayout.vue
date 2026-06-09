0aed3554eaa511c3670568c98dc40bad2549f42c
Ugur Kaya <ugurkaya67@hotmail.com>
2026-06-09 13:04:20 +0200

    feat: add reusable app layout component

---
+++ inertia/layouts/AppLayout.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import Footer from '~/components/Footer.vue';
+import Header from '~/components/Header.vue';
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-white text-black">
+    <Header />
+
+    <main>
+      <slot />
+    </main>
+
+    <Footer />
+  </div>
+</template>
